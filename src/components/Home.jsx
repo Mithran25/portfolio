3d8109e25bc6691dc99d3da2deefe9c0b0b7bf3d
Mithran25 <navamithran25@gmail.com>
2025-08-06 20:51:13 +0530

    seventh update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import resume from "./Navamithran_resume.pdf"
+import resume from "./Navamithran_Resume.pdf"
 function Home(){
 
     const[hero,setHero]=useState(false);    
@@ -33,7 +33,7 @@ function Home(){
 
               <div className="resume">
               <a href="mailto:navamithran25@gmail.com">Hire Me</a>
-              <a href={resume}>Resume</a>
+              <a href={resume} download>Resume</a>
               </div>
         
              <div className="main_content_icon">
